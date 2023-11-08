69bdabe8fc09907dbe82dc922adce6aa00576455
tada5hi <peter.placzek1996@gmail.com>
2023-11-08 12:57:11 +0100

    fix: change color generation for string

--- packages/core/src/utils/color.ts
+++ packages/core/src/utils/color.ts
@@ -4,14 +4,16 @@ export function generateRandomColor() {
 
 export function stringToColor(str: string) {
     let hash = 0;
-    str.split('').forEach((char) => {
-        hash = char.charCodeAt(0) + ((hash << 5) - hash);
-    });
-    let colour = '#';
-    for (let i = 0; i < 3; i++) {
-        const value = (hash >> (i * 8)) & 0xff;
-        colour += value.toString(16).padStart(2, '0');
+    for (let i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
     }
+    return `#${intToRGB(hash)}`;
+}
 
-    return colour;
+function intToRGB(i: number) {
+    const c = (i & 0x00FFFFFF)
+        .toString(16)
+        .toUpperCase();
+
+    return '00000'.substring(0, 6 - c.length) + c;
 }
