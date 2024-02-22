604ce411a471f4a5c8d1c3468988750f3ba73700
tada5hi <peter.placzek1996@gmail.com>
2024-02-22 12:15:54 +0100

    fix: from select listener

--- packages/core/src/components/core/query/QuerySummaryGrouped.vue
+++ packages/core/src/components/core/query/QuerySummaryGrouped.vue
@@ -31,7 +31,7 @@ export default defineComponent({
         })));
 
         watch(id, (val) => {
-            if (val === '') {
+            if (typeof val !== 'string' || val === '') {
                 item.value = null;
                 return;
             }
