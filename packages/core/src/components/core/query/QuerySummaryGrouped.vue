a67fdaef1888ae9a3331c5dec6fe1a79ad3b4386
tada5hi <peter.placzek1996@gmail.com>
2024-02-22 11:38:36 +0100

    fix: selection of first element in summary-grouped component

--- packages/core/src/components/core/query/QuerySummaryGrouped.vue
+++ packages/core/src/components/core/query/QuerySummaryGrouped.vue
@@ -31,7 +31,7 @@ export default defineComponent({
         })));
 
         watch(id, (val) => {
-            if (!val) {
+            if (val === '') {
                 item.value = null;
                 return;
             }
