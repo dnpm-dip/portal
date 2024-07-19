2694bb5281f3ee9cd5cd1c8f1d774142dd9c4b4a
Peter <peter.placzek1996@gmail.com>
2024-07-19 09:59:41 +0200

    feat: unified component for query criteria summary (#627)

--- packages/mtb/src/runtime/components/core/MQueryPatientMatch.vue
+++ packages/mtb/src/runtime/components/core/MQueryPatientMatch.vue
@@ -1,12 +1,12 @@
 <script lang="ts">
-import { DCodingText } from '@dnpm-dip/core';
 import type { PropType } from 'vue';
 import { defineComponent, ref } from 'vue';
 import type { PatientMatch } from '../../domains';
+import MQueryCriteriaSummary from './MQueryCriteriaSummary.vue';
 
 export default defineComponent({
     components: {
-        DCodingText,
+        MQueryCriteriaSummary,
     },
     props: {
         entity: {
@@ -57,10 +57,13 @@ export default defineComponent({
                 </NuxtLink>
             </div>
         </div>
-        <div :class="{'d-flex flex-row': extended}">
+        <div
+            class=" mb-2 mt-2"
+            :class="{'d-flex flex-row': extended}"
+        >
             <div
-                class="d-flex justify-content-between mb-2 mt-2"
-                :class="{'flex-row': !extended, 'flex-column': extended}"
+                class="d-flex justify-content-between"
+                :class="{'flex-row': !extended, 'flex-column me-3': extended}"
             >
                 <div
                     v-if="entity.managingSite"
@@ -116,159 +119,9 @@ export default defineComponent({
                     </div>
                 </div>
             </div>
-            <div
-                v-if="extended"
-                class="ms-3 d-flex flex-column justify-content-between mb-2 mt-2"
-            >
-                <div v-if="entity.matchingCriteria.diagnoses">
-                    <strong>Diagnose</strong>
-
-                    <template
-                        v-for="item in entity.matchingCriteria.diagnoses"
-                        :key="item.code"
-                    >
-                        <span class="badge bg-dark ms-1"><DCodingText
-                            :composite="true"
-                            :entity="item"
-                        /></span>
-                    </template>
-                </div>
-
-                <div v-if="entity.matchingCriteria.tumorMorphologies">
-                    <strong>Tumor-Morphologie</strong>
-
-                    <template
-                        v-for="item in entity.matchingCriteria.tumorMorphologies"
-                        :key="item.code"
-                    >
-                        <span class="badge bg-dark ms-1"><DCodingText
-                            :composite="true"
-                            :entity="item"
-                        /></span>
-                    </template>
-                </div>
-
-                <div v-if="entity.matchingCriteria.medication">
-                    <strong>Medikation</strong>
-
-                    <template v-if="entity.matchingCriteria.medication.drugs">
-                        <template
-                            v-for="item in entity.matchingCriteria.medication.drugs"
-                            :key="item.code"
-                        >
-                            <span class="badge bg-dark ms-1">{{ item.display || item.code }}</span>
-                        </template>
-                    </template>
-                </div>
-
-                <div v-if="entity.matchingCriteria.responses">
-                    <strong>Response</strong>
-
-                    <template
-                        v-for="item in entity.matchingCriteria.responses"
-                        :key="item.code"
-                    >
-                        <span class="badge bg-dark ms-1">{{ item.display || item.code }}</span>
-                    </template>
-                </div>
-            </div>
-            <div
-                v-if="extended"
-                class="ms-3 d-flex flex-column justify-content-between mb-2 mt-2"
-            >
-                <template v-if="entity.matchingCriteria.simpleVariants">
-                    <template
-                        v-for="item in entity.matchingCriteria.simpleVariants"
-                        :key="item.code"
-                    >
-                        <div>
-                            <strong>SNV</strong>
-
-                            <ul class="list-unstyled">
-                                <li v-if="item.gene">
-                                    <strong>&bull; Gen</strong> <DCodingText
-                                        :entity="item.gene"
-                                    />
-                                </li>
-                                <li v-if="item.dnaChange">
-                                    <strong>&bull; DNA-Änderung</strong> <DCodingText
-                                        :entity="item.dnaChange"
-                                    />
-                                </li>
-                                <li v-if="item.proteinChange">
-                                    <strong>&bull; Proteinänderung</strong> <DCodingText
-                                        :entity="item.proteinChange"
-                                    />
-                                </li>
-                            </ul>
-                        </div>
-                    </template>
-                </template>
-                <template v-if="entity.matchingCriteria.copyNumberVariants">
-                    <template
-                        v-for="(item, key) in entity.matchingCriteria.copyNumberVariants"
-                        :key="key"
-                    >
-                        <div>
-                            <strong>CNV</strong>
-
-                            <ul class="list-unstyled">
-                                <li v-if="item.type">
-                                    <strong>&bull; Type</strong>
-                                    {{ item.type.display || item.type.code }}
-                                </li>
-                                <li v-if="item.affectedGenes">
-                                    <strong>&bull; Betroffene Gene</strong>
-                                    <template
-                                        v-for="el in item.affectedGenes"
-                                        :key="el.code"
-                                    >
-                                        <span class="badge bg-dark ms-1">{{ el.display || el.code }}</span>
-                                    </template>
-                                </li>
-                            </ul>
-                        </div>
-                    </template>
-                </template>
-                <template v-if="entity.matchingCriteria.dnaFusions">
-                    <template
-                        v-for="(item, key) in entity.matchingCriteria.dnaFusions"
-                        :key="key"
-                    >
-                        <div>
-                            <strong>DNA-Fusion</strong>
-
-                            <ul class="list-unstyled">
-                                <li v-if="item.fusionPartner3pr">
-                                    <strong>&bull; 3'-Gen</strong> {{ item.fusionPartner3pr }}
-                                </li>
-                                <li v-if="item.fusionPartner5pr">
-                                    <strong>&bull; 5'-Gen</strong> {{ item.fusionPartner5pr }}
-                                </li>
-                            </ul>
-                        </div>
-                    </template>
-                </template>
-                <template v-if="entity.matchingCriteria.rnaFusions">
-                    <template
-                        v-for="(item, key) in entity.matchingCriteria.rnaFusions"
-                        :key="key"
-                    >
-                        <div>
-                            <strong>RNA-Fusion</strong>
-
-                            <ul class="list-unstyled">
-                                <li v-if="item.fusionPartner3pr">
-                                    <strong>&bull; 3'-Gen</strong> {{ item.fusionPartner3pr }}
-                                </li>
-                                <li v-if="item.fusionPartner5pr">
-                                    <strong>&bull; 5'-Gen</strong> {{ item.fusionPartner5pr }}
-                                </li>
-                            </ul>
-                        </div>
-                    </template>
-                </template>
-            </div>
+            <template v-if="extended">
+                <MQueryCriteriaSummary :entity="entity.matchingCriteria" />
+            </template>
         </div>
     </div>
 </template>
