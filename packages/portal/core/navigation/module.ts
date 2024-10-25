6bb2913036994c5f970812e5c51af1eb24b678d0
tada5hi <peter.placzek1996@gmail.com>
2024-10-25 09:41:46 +0200

    style: adjusted navigation styling

--- packages/portal/core/navigation/module.ts
+++ packages/portal/core/navigation/module.ts
@@ -29,7 +29,7 @@ export class Navigation {
         this.topElements = [
             {
                 name: TopDefaultName,
-                icon: 'fa fa-home',
+                url: '/',
             },
         ];
 
