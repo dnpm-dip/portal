5a5bc6dc8935113e4083227bd5f27730ef5eb163
tada5hi <peter.placzek1996@gmail.com>
2025-11-17 15:37:40 +0100

    feat: show total in response distribution bar

--- packages/portal/plugins/bootstrap.ts
+++ packages/portal/plugins/bootstrap.ts
@@ -1,10 +1,17 @@
-import { createBootstrap } from 'bootstrap-vue-next';
+import { Directives, createBootstrap } from 'bootstrap-vue-next';
 import { defineNuxtPlugin } from '#app';
 
 export default defineNuxtPlugin({
     async setup(nuxt) {
-        nuxt.vueApp.use(createBootstrap({
-            toast: true,
-        }));
+        nuxt.vueApp.use(createBootstrap());
+
+        const keys = Object.keys(Directives);
+        for (let i = 0; i < keys.length; i++) {
+            const name = keys[i];
+
+            if (name) {
+                nuxt.vueApp.directive(name.replace(/^v/, ''), Directives[name as keyof typeof Directives]);
+            }
+        }
     },
 });
