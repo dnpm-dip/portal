9010f5f41fd57241affae8304e3076a6469e0b2e
Peter <peter.placzek1996@gmail.com>
2026-03-26 13:04:54 +0100

    feat(mtb): support QueryGeneAlteration object keys in variant distributions (#1191)

--- packages/mtb/src/runtime/domains/query/helper.ts
+++ packages/mtb/src/runtime/domains/query/helper.ts
@@ -5,7 +5,25 @@
  * view the LICENSE file that was distributed with this source code.
  */
 
-import type { QueryGeneAlterationCriteria } from './types';
+import type { Coding } from '@dnpm-dip/core';
+import type { QueryGeneAlteration, QueryGeneAlterationCriteria } from './types';
+
+function codingToString(coding: Coding): string {
+    return coding.display || coding.code;
+}
+
+export function queryGeneAlterationToString(entity: QueryGeneAlteration): string {
+    switch (entity.type) {
+        case 'SNV':
+            return `${codingToString(entity.gene)} ${entity.proteinChange || 'SNV'}`;
+        case 'CNV':
+            return `${codingToString(entity.gene)} ${codingToString(entity.copyNumberType)}`;
+        case 'Fusion':
+            return `${codingToString(entity.gene)}-${codingToString(entity.partner)} Fusion`;
+        default:
+            return '???';
+    }
+}
 
 export function buildQueryGeneAlterationCriteriaLabel(criteria: QueryGeneAlterationCriteria) {
     if (!criteria.gene) {
@@ -14,7 +32,7 @@ export function buildQueryGeneAlterationCriteriaLabel(criteria: QueryGeneAlterat
 
     const parts : string[] = [];
 
-    parts.push(criteria.gene.display || criteria.gene.code);
+    parts.push(codingToString(criteria.gene));
 
     if (criteria.alteration) {
         parts.push(criteria.alteration.type);
