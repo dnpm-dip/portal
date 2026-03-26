9c744b41e7df3a012ca5feec097a98ba53cf0416
tada5hi <peter.placzek1996@gmail.com>
2026-03-26 15:03:14 +0100

    fix: rendering gene alteration as string

--- packages/mtb/src/runtime/domains/query/helper.ts
+++ packages/mtb/src/runtime/domains/query/helper.ts
@@ -16,10 +16,18 @@ export function queryGeneAlterationToString(entity: QueryGeneAlteration): string
     switch (entity.type) {
         case 'SNV':
             return `${codingToString(entity.gene)} ${entity.proteinChange || 'SNV'}`;
-        case 'CNV':
-            return `${codingToString(entity.gene)} ${codingToString(entity.copyNumberType)}`;
-        case 'Fusion':
-            return `${codingToString(entity.gene)}-${codingToString(entity.partner)} Fusion`;
+        case 'CNV': {
+            const cnvType = typeof entity.copyNumberType === 'string' ?
+                entity.copyNumberType :
+                codingToString(entity.copyNumberType);
+            return `${codingToString(entity.gene)} ${cnvType || 'CNV'}`;
+        }
+        case 'Fusion': {
+            const partner = typeof entity.partner === 'string' ?
+                entity.partner :
+                codingToString(entity.partner);
+            return `${codingToString(entity.gene)}-${partner || '?'} Fusion`;
+        }
         default:
             return '???';
     }
